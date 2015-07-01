299e3edb7344d1c5ab2963868ced9352f6ed135e
David Tsang <davidytsang@gmail.com>
2015-07-01 00:16:56 -0400

    complete dungeon game

--- dungeon_game.py
+++ dungeon_game.py
@@ -3,32 +3,95 @@ CELLS = [(0, 0), (0, 1), (0, 2),
          (1, 0), (1, 1), (1, 2),
          (2, 0), (2, 1), (2, 2)]
 
-def get_location(arg):
+def get_locations():
+    monster =  random.choice(CELLS)
+    door = random.choice(CELLS)
+    start = random.choice(CELLS)
+
+    if monster == door or monster == start or door == start:
+        return get_locations()
+    return monster, door, start
+
     # monster =  random
     # door = random
     # start = random
 
     # if monster, door , or start are the same do it again
 def move_player(player, move):
+    # player = (x, y)
+    x, y = player
+    if move == 'LEFT':
+        y -= 1
+    elif move == 'RIGHT':
+        y += 1
+    elif move == 'UP':
+        x -= 1
+    elif move == 'DOWN':
+        x += 1
+
+    return x ,y
     # Get player's current location
     # If move is LEFT y - 1
     # If move is RIGHT y + 1
     # If move is UP x - 1
     # If move is Down x + 1
-def get_moves(player, move):
-    MOVES = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+def get_moves(player):
+    moves = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+    # player = (x, y)
+    if player[1] == 0:
+        moves.remove('LEFT')
+    if player[1] == 2:
+        moves.remove('RIGHT')
+    if player[0] == 0:
+        moves.remove('UP')
+    if player[0] == 2:
+        moves.remove('DOWN')
     # if player's y i 0 remove LEFT
     # if player's x i 0 remove LEFT
     # if player's y i 2 remove LEFT
     # if player's x i 0 remove LEFT
+    return moves
+
+def draw_map(player):
+    print(' _  _  _')
+    tile = '|{}'
+
+    for idx, cell in enumerate(CELLS):
+        if idx in [0, 1, 3, 4, 6, 7]:
+            if cell == player:
+                print(tile.format('X')),
+            else:
+                print(tile.format('_')),
+        else:
+            if cell == player:
+                print(tile.format('X|'))
+            else:
+                print(tile.format('_|'))
+
+monster, door, player = get_locations()
+print("Welcome to the dungeon!")
+
 while True:
-    print("Welcome to the dungeon!")
-    print("You're current;y in room {}")
-    print("You can move {}")
+    moves = get_moves(player)
+
+    print("You're current;y in room {}".format(player))
+    draw_map(player)
+    print("You can move {}".format(moves))
     print("ENTER QUIT to quit")
 
-    move = raw.input("> ")
+    move = raw_input("> ")
     move = move.upper()
 
     if move == 'QUIT':
+        break
+    if move in moves:
+        player = move_player(player, move)
+    else:
+        print("** Walls are hard, stop walkign into them! **")
+        continue
+
+    if player == door:
+        print("You escaped!")
+    elif player == monster:
+        print("You were eaten by the grue!")
         break
